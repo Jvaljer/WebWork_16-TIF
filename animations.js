55ec1ef040cf4f668b6f3512fa80b1ae10b6ee83
Abel Henry-Lapassat <98323161+Jvaljer@users.noreply.github.com>
2025-02-02 15:24:05 +0100

    struggling with scrollTrigger ...

--- animations.js
+++ animations.js
@@ -1,8 +1,9 @@
 import gsap from 'gsap';
 import Flip from 'gsap/Flip';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(Flip);
-gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 let opening = gsap.timeline({ paused: true});
 
@@ -35,14 +36,6 @@ opening.to('#opening', { opacity: 1.0, delay: 1.5, duration: 2})
 let home = gsap.timeline({ paused: true});
 
 home.to('#home', {display: 'flex', duration: 0.5, ease: 'power1.out'})
-.to('#home', {
-    opacity: 1,
-    duration: 2,
-    ease: 'power1.out',
-    onStart: () => {
-        reParent();
-    }
-})
 .to('#hold-button', {
     opacity: 0,
     duration: 0.5,
@@ -51,19 +44,71 @@ home.to('#home', {display: 'flex', duration: 0.5, ease: 'power1.out'})
         document.getElementById('hold-button').style.display = 'none';
     }
 }, '<')
+.to('#home', {
+    opacity: 1,
+    duration: 2,
+    ease: 'power1.out',
+    onStart: () => {
+        reParent();
+    }
+},'<')
 .from('#home-image', {
-    height: '4px',
+    height: '0px',
     duration: 1,
     ease: 'power1.out',
 })
+.from('#credits', {
+    opacity: 0,
+    x: 500,
+    duration: 2,
+    ease: 'power1.out',
+})
+.to('#scroll-indicator', {
+    display: 'flex',
+    duration: 0.1,
+    ease: 'power1.out',
+    onComplete: () => {
+        scroll.play();
+        ScrollTrigger.refresh();
+    }
+})
+.to('#scroll-indicator', {
+    opacity: 1,
+    duration: 0.5,
+    ease: 'power1.out',
+})
 
+let scroll = gsap.timeline({ paused: true, repeat: -1, repeatDelay: 0.05 });
+
+scroll.to('.scroll-arrow:nth-child(1)', {
+    opacity: 1,
+    y: 4,
+    duration: 0.5,
+    ease: 'power1.out',
+})
+.to('.scroll-arrow:nth-child(2)', {
+    opacity: 1,
+    y: 4,
+    duration: 0.5,
+    ease: 'power1.out',
+}, '-=0.25')
+.to('.scroll-arrow:nth-child(3)', {
+    opacity: 1,
+    y: 4,
+    duration: 0.5,
+    ease: 'power1.out',
+}, '-=0.25')
+
+let homeScroll = gsap.timeline({
+    scrollTrigger: {
+        // TODO: fix this...
+    }
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded");
     opening.play();
 })
-
-const openingTexts = document.querySelector('.opening-text');
 
 const tif = document.querySelector('#tif-text');
 const session = document.querySelector('#session-text');
@@ -98,8 +143,6 @@ startButton.addEventListener('click', () => {
     const buttonWidth = computedButton.width;
 
     if (beforeWidth === buttonWidth) {
-        console.log("home.play()");
         home.play();
     }
 })
-
