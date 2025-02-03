dd8f97402511b61e6608b94199d78f31461eba3e
Abel Henry-Lapassat <98323161+Jvaljer@users.noreply.github.com>
2025-02-03 11:52:54 +0100

    worked out the home-bar scroll animation

---
+++ script.js
@@ -0,0 +1,63 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const opening = gsap.timeline({paused:true});
+
+const home = gsap.timeline({paused:true});
+
+
+const homeScroll = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home-bar',
+        start: 'top-=32 top',
+        scrub: 1,
+        markers: true
+    },
+    paused:true
+});
+
+
+homeScroll.to('.home-bar', {
+    position: 'fixed',
+    top: '32px',
+    opacity: 0.5,
+    ease: 'power1.out',
+})
+.to('.bar-image', {
+    height: '4px',
+    ease: 'power1.out',
+}, '<')
+.to('#logo', {
+    height: '2vw',
+    ease: 'power1.out',
+}, '<')
+.to('#session-text', {
+    fontSize: '1.25vw',
+    ease: 'power1.out',
+}, '<')
+.to('.bar-top', {
+    height: '2vw',
+    ease: 'power1.out',
+}, '<')
+.to('.bar-bottom', {
+    opacity: 0,
+    ease: 'power1.out',
+}, '<')
+.to('#tif-text', {
+    fontSize: '1.5vw',
+    opacity: 0,
+    ease: 'power1.out',
+}, '<')
+.to('#credit-text', {
+    fontSize: '1vw',
+    opacity: 0,
+    ease: 'power1.out',
+}, '<')
+.to('.bar-bottom', {
+    display: 'none',
+    ease: 'power1.out',
+})
+
+
